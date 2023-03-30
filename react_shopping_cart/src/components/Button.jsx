14ce07eb30fcee6cf6279b2f5dff5f6406fc0d92
AustenVanBrogen <austenvanbrogen@yahoo.com>
2023-03-30 18:17:01 -0500

    Made the navbar move with the page. Started adding functionality for changing the navbar and page when pressing the button, but it's not working yet

--- react_shopping_cart/src/components/Button.jsx
+++ react_shopping_cart/src/components/Button.jsx
@@ -1,10 +1,18 @@
 import './Button.css';
+import React, {useState, useEffect} from 'react';
 
-export default function Button({button}){
+export default function Button({button}, {clickedBtn}){
+
+    const [active, setActive] = useState('');
 
     function handleClick(){
-        console.log(getButtonIDs()[1]);
+        //console.log(getButtonIDs()[1]);
+        //changeSelectedBtn();
+
+        clickedBtn(getButtonIDs()[1]) //For some reason is says this passed function doesn't exist. Start here.
+
     }
+
     function getButtonIDs(){
         let stringArray = button.toString().split(" ");
         if(stringArray[1] === 'decrement'){
@@ -17,7 +25,21 @@ export default function Button({button}){
             return stringArray;
         }
     }
-    return <div className={button} onClick={handleClick}>
+
+    function changeSelectedBtn(){
+        if(active != 'active'){
+            setActive('active');
+        }
+        else{
+            setActive('');
+        }
+    }
+
+    // useEffect(() => {
+    //     console.log("A button was clicked");
+    // }, [clickedBtn])
+
+    return <div className={`${button} ${active}`} onClick={handleClick}>
         <h1>{getButtonIDs()[1]}</h1>        
     </div>
 }
--- react_shopping_cart/src/components/Navbar.jsx
+++ react_shopping_cart/src/components/Navbar.jsx
@@ -1,11 +1,19 @@
 import './Navbar.css';
 import Button from './Button.jsx';
+import React, {useState} from 'react';
 
 export default function Navbar(){
+    const [activeBtn, setActiveBtn] = useState('Jackets');
+
+    function buttonClicked(btn){
+        //setActiveBtn(btn);
+        console.log(`${btn} was clicked`);
+    }
+
     return <div className='navBar'>
-        <Button button='navbarButton Jackets'></Button>
-        <Button button='navbarButton Tops'></Button>
-        <Button button='navbarButton Boots'></Button>
-        <Button button='navbarButton Pants'></Button>
+        <Button button='navbarButton Jackets' clickedBtn={buttonClicked}></Button>
+        <Button button='navbarButton Tops' clickedBtn={buttonClicked}></Button>
+        <Button button='navbarButton Boots' clickedBtn={buttonClicked}></Button>
+        <Button button='navbarButton Pants' clickedBtn={buttonClicked}></Button>
         </div>
 }
